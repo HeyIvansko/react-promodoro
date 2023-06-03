0370f673be2d392d2e6900b6ffcf5194ef2d6332
Ivansko <ivanxsoumah@gmail.com>
2023-06-03 17:19:18 +0000

    code source

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState(1500); // Durée initiale en secondes (25 minutes)
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive && time > 0) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime - 1);
+      }, 1000);
+    } else if (!isActive && time === 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  const startTimer = () => {
+    setIsActive(true);
+  };
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setTime(1500);
+  };
+
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pomodoro </h1>
+      <div className={`timer ${isActive ? 'active' : ''}`}>{formatTime()}</div>
+      <div className="controls">
+        {!isActive ? (
+          <button className="start" onClick={startTimer}>Start</button>
+        ) : (
+          <button className="pause" onClick={() => setIsActive(false)}>Pause</button>
+        )}
+        <button className="reset" onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
